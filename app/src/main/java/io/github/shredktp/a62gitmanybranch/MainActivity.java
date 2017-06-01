3f8f60899d5e5e5af344f4b9b110da65fecec5c1
shredktp <kornth_ebasicxp@hotmail.com>
2017-06-01 14:16:01 +0700

    Change branch name, change title name

--- app/src/main/java/io/github/shredktp/a62gitmanybranch/MainActivity.java
+++ app/src/main/java/io/github/shredktp/a62gitmanybranch/MainActivity.java
@@ -1,10 +1,15 @@
 package io.github.shredktp.a62gitmanybranch;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String GIT_BRANCH_NAME = "develop";
+    private static final String TAG = "MainActivity";
 
     TextView tvBranchName;
 
@@ -13,8 +18,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setupTitleName();
+
         tvBranchName = (TextView) findViewById(R.id.tv_branch_name);
         String branchName = getString(R.string.tv_branch_name);
         tvBranchName.setText(branchName);
+    }
+
+    private void setupTitleName() {
+        String titleName = getString(R.string.app_name) + ": " + GIT_BRANCH_NAME;
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            Log.i(TAG, "setupTitleName: Changed");
+            actionBar.setTitle(titleName);
+        } else {
+            Log.i(TAG, "setupTitleName: null");
+        }
     }
 }
